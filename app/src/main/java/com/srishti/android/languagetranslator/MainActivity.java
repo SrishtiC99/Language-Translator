124fcf6cee72c6973c009d65cb2c78024cd97eac
Srishti Kumari <sri993951@gmail.com>
2021-12-11 09:12:58 +0530

    Added code for translation

--- app/src/main/java/com/srishti/android/languagetranslator/MainActivity.java
+++ app/src/main/java/com/srishti/android/languagetranslator/MainActivity.java
@@ -1,14 +1,180 @@
 package com.srishti.android.languagetranslator;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Spinner fromSpinner, toSpinner;
+    private TextInputEditText sourceET;
+    private ImageView micIV;
+    private MaterialButton translateButton;
+    private TextView translatedTV;
+    String[] fromLanguages = {"From", "English"};
+    String[] toLanguages = {"To", "Hindi"};
+    private static final int REQUEST_PERMISSION_CODE = 1;
+    int languageCode, fromLanguageCode, toLanguageCode = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fromSpinner = findViewById(R.id.from_spinner);
+        toSpinner = findViewById(R.id.to_spinner);
+        sourceET = findViewById(R.id.source_et);
+        micIV = findViewById(R.id.voice_mic);
+        translateButton = findViewById(R.id.translate_button);
+        translatedTV = findViewById(R.id.translated_tv);
+
+        fromSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                fromLanguageCode = getLanguageCode(fromLanguages[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayAdapter fromAdapter = new ArrayAdapter(this, R.layout.spinner_item, fromLanguages);
+        fromAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        fromSpinner.setAdapter(fromAdapter);
+
+        toSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                toLanguageCode = getLanguageCode(toLanguages[i]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        ArrayAdapter toAdapter = new ArrayAdapter(this, R.layout.spinner_item, toLanguages);
+        toAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        toSpinner.setAdapter(fromAdapter);
+
+        translateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                translatedTV.setText("");
+                if(sourceET.getText().toString().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please enter your text to translate", Toast.LENGTH_LONG).show();
+                }
+                else if(fromLanguageCode == 0){
+                    Toast.makeText(MainActivity.this, "Please select source language", Toast.LENGTH_LONG).show();
+                }
+                else if(toLanguageCode == 0){
+                    Toast.makeText(MainActivity.this, "Please select language to make translation", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    translateText(fromLanguageCode, toLanguageCode, sourceET.getText().toString());
+                }
+            }
+        });
+
+        micIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to convert into text");
+                try{
+                    startActivityForResult(i, REQUEST_PERMISSION_CODE);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "" + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_PERMISSION_CODE){
+            if(resultCode == RESULT_OK && data != null){
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                sourceET.setText(result.get(0));
+            }
+        }
+    }
+
+    private void translateText(int fromLangCode, int toLangCode, String source){
+        translatedTV.setText("Downloading...");
+        FirebaseTranslatorOptions options = new FirebaseTranslatorOptions.Builder()
+                .setSourceLanguage(fromLangCode)
+                .setTargetLanguage(toLangCode)
+                .build();
+        FirebaseTranslator translator = FirebaseNaturalLanguage.getInstance().getTranslator(options);
+
+        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder().build();
+
+        translator.downloadModelIfNeeded(conditions).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                translatedTV.setText("Translating...");
+                translator.translate(source).addOnSuccessListener(new OnSuccessListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        translatedTV.setText(s);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Fail to translate: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Fail to download language model: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private int getLanguageCode(String lang){
+        int langCode = 0;
+        switch (lang){
+            case "English":
+                langCode = FirebaseTranslateLanguage.EN;
+                break;
+            case "Hindi":
+                langCode = FirebaseTranslateLanguage.HI;
+                break;
+        }
+        return langCode;
     }
 }
